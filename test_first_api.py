e69087433e2b2a1b00d8622f50a46d629f7e63f3
ZhiYuanSu0159 <1306106352@qq.com>
2026-08-04 22:11:59 +0800

    第三天：#今日20260803了解使用pytest执行脚本，感受了下失败提示，理解pytest执行逻辑，需要test_函数或者类方法中包裹

--- test_first_api.py
+++ test_first_api.py
@@ -1,21 +1,20 @@
 import requests
 
-url = "https://jsonplaceholder.typicode.com/posts/1"
 
-response = requests.get(url)
+def test_first_api():
+    url = "https://jsonplaceholder.typicode.com/posts/1"
+    response = requests.get(url)
 
+    assert response.status_code == 200, f"状态码错误，预期200实际{response.status_code}"
 
-assert response.status_code == 200, f"状态码错误，预期200实际{response.status_code}"
+    data = response.json()
 
-data = response.json()
+    assert "userId" in data, "返回数据缺少userID"
+    assert "id" in data, "返回数据缺少 id 字段"
+    assert "title" in data, "返回数据缺少 title 字段"
+    assert "body" in data, "返回数据缺少 body 字段"
 
-assert "userId" in data, "返回数据缺少userID"
-assert "id" in data, "返回数据缺少 id 字段"
-assert "title" in data, "返回数据缺少 title 字段"
-assert "body" in data, "返回数据缺少 body 字段"
+    assert data["id"] == 1, f"帖子 id 错误，预期1，实际{data['id']}"
 
-
-assert data["id"] == 1,f"帖子 id 错误，预期1，实际{data['id']}"
-
-print("PASS")
-print(f"帖子标题: {data['title']}")
+    print("PASS")
+    print(f"帖子标题: {data['title']}")
